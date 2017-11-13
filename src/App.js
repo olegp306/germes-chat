35cb2fca1563dd85b16d6e694a55d0fc97b446e0
olegp306 <olegp306@gmail.com>
2017-11-13 22:20:15 +0600

    НЕ РАБОТАЕТ НО ФИКСИРУЮ

--- src/App.js
+++ src/App.js
@@ -6,70 +6,137 @@ import Chat from './components/Chat.jsx';
 
 
 class App extends Component {
+  constructor(props) {
+    console.log('constructor');
+    super(props);
+    this.state={};
 
-  componentWillMount() {
+    //this.state = {};
+  }
+  componentDidMount() {
     let baseurl='http://localhost:85/germes/v1';
-    //http://localhost:85/germes/v1/users/chatid/2768027587000
-    var chatdata={};
     let requestId=this.props.params.requestId;
-
     let userId=this.props.params.userId;
+    var chatData={};
+    var axiosChat = axios.create({baseURL: 'http://localhost:85/germes/v1'});
+    this.setState({test1:'beforegetTokenByUserId' });
 
-//получим токен
-axios.post(baseurl+'/authbyappkey/token', {
-  userid: userId,
-  appkey: "kdwcc83defm8o7bkdwcc83defm8o7b"
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+    var autorizeByUserId = function (userId){
+      return axiosChat.post(baseurl+'/authbyappkey/token', {
+        userid: userId,
+        appkey: "kdwcc83defm8o7bkdwcc83defm8o7b"
+        })
+      // .then(function (response) {
+      //   console.log(response);
+      // })
+      // .catch(function (error) {
+      //   console.log(error);
+      // });
+    }
 
+    var getChatInfoByRequestId=function (requestId){
+      return  axiosChat.get(baseurl+'/chats/request/'+requestId)
+      // .then(function (response) {
+      //   console.log(' getChatInfoByRequestId',response);
+      // })
+      // .catch(function (error) {
+      //   console.log(' getChatInfoByRequestId',error);
+      // });
+    }
 
-//найдем чат по id заявки
-    axios.get(baseurl+'/chats/request/'+requestId)
-    .then(function (response) {
-      console.log('axios',response);
-    })
-    .catch(function (error) {
-      console.log('axios',error);
-    });
+    var getUsersByChatId=function (chatId) {
+      return axiosChat.get(baseurl+'/users/chatid/'+this.chatId)
+      // .then(function (response) {
+      //   console.log(' getCurrentChat',response);
+      // })
+      // .catch(function (error) {
+      //   console.log(' getCurrentChat',error);
+      // });
+    }
 
-//соберем все данные по заявке
+    var getAllChatsByUserId= function (userId) {
+      return axiosChat.get(baseurl+'/chats/user/'+this.userId)
+      // .then(function (response) {
+      //   console.log(' getCurrentChat',response);
+      // })
+      // .catch(function (error) {
+      //   console.log(' getCurrentChat',error);
+      // });
+    }
 
-      function getCurrentChat() {
-        return axios.get(baseurl+'/users/chatid/2768027587000');
+    var getAllUsersByChatId= function (chatId) {
+      return axiosChat.get(baseurl+'/users/chatid/'+chatId)
+      // .then(function (response) {
+      //   console.log(' getUsers',response);
+      // })
+      // .catch(function (error) {
+      //   console.log(' getUsers',error);
+      // });;
+    }
+
+    var getMessagesByChatId=function (chatId) {
+      return axiosChat.get(baseurl+'/messages/chatid/'+ chatId)
+      // .then(function (response) {
+      //   console.log(' getMessages',response);
+      // })
+      // .catch(function (error) {
+      //   console.log(' getMessages',error);
+      // });
+    }
+
+    var toAssociativeArrfunction =function (data){
+      let map = {};
+      //console.log('toAssociativeArr', data);
+      for (var i = 0, l = data.length; i < l; i++) {
+          map[data[i].id] = data[i];
       }
+      //console.log(map);
+      return map;
+    }
 
-      function getUsers() {
-        return axios.get(baseurl+'/users/chatid/2768027587000');
-      }
+    var getInitialData=function (){
 
-      function getMessages() {
-        return axios.get(baseurl+'/germes/v1/messages/'+ chatdata.currentChatId);
-      }
+      autorizeByUserId(userId)
+      .then((response)=>{
+        axiosChat.defaults.headers.common['Authorization'] = 'Bearer '+response.data.accessToken;
+        //2768005082000
+        return getChatInfoByRequestId(requestId)
+      })
+      .then((response)=>{
+        chatData.currentChatInfo=response.data;
+        //console.log('chatData.currentChatId=response.data.Id:',response);
+      })
+      .then((response)=>{
+        axios.all([getMessagesByChatId(chatData.currentChatInfo.id), getAllUsersByChatId(chatData.currentChatInfo.id)])
+          .then(axios.spread(function (messages, users) {
+            chatData.messages=toAssociativeArr(messages.data);
+            chatData.users=toAssociativeArr(users.data);
+            //chatData.chats=
+            chatData.currentChatId=chatData.currentChatInfo.id;
+            chatData.currentUserId=userId;
+        }))
+        .then((response)=>{
+          this.setState({chat:chatData});
+          console.log(chatData);
 
+        })
 
-      axios.all([getUsers(), getMessages()])
-      .then(axios.spread(function (users, messages) {
-        chatdata.users=users;
-        chatdata.messages=messages;
-          console.log(chatdata);
-      }));
-
-  //console.log(chatdata);
-}
-
-
+        console.log(chatData);
+      });
+  }
+    getInitialData();
+  }
 
   render() {
-    //console.log('console from APP');
+  console.log('render');
+  if(!this.state.alldata)
+  {
+    return <p>Loading</p>
+  }
     //console.log(data);
     return (
       <div className="App">
-        <Chat data={data}/>
+        <Chat data={this.state.alldata}/>
       </div>
     );
   }
