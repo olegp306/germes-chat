7bfa52678ce20c534e093fb6dde8fb38ac3e7d84
olegp306 <olegp306@gmail.com>
2017-11-22 19:16:16 +0600

    Разложил полуение и изменение данных по методам жизненного цикла компонента

--- src/components/MessagesList.jsx
+++ src/components/MessagesList.jsx
@@ -1,31 +1,76 @@
 import React, { Component } from 'react';
+
+import NewMessageBox from './NewMessageBox.jsx';
 import Message from './Message.jsx';
+import * as api from  '../api/api.js';
 
 class MessagesList extends Component {
-    render() {
-    //console.log(this.props.data);
-    let currentUserId=this.props.currentUserId;
-    let users=this.props.users;
-    let messagesList= new Array;
+  constructor(props) {
+    super(props);
+    this.state={};
+  }
 
-    for (let prop in this.props.messages)
-    {
-      let message=this.props.messages[prop];
+ //при инициализации 1 раз
+  componentDidMount() {
+    this.setState({users:this.props.users,messages:this.props.messages, currentUserId:this.props.currentUserId}) ;
+    //this.scrollTolastMessage();
+  }
 
-      let isMyMessage=false;
-      if(message.userId==currentUserId){
-        isMyMessage=true;
-      }
+  componentWillReceiveProps(nextProps) {
+    console.log(nextProps);
+    //this.state={text:nextProps.newMessageText};
+    this.setState({users:nextProps.users,messages:nextProps.messages, currentUserId:nextProps.currentUserId}) ;
+  }
+  //каждый раз после изменения props после render
+  componentDidUpdate(prevProps, prevState) {
+    this.scrollTolastMessage();
+  }
 
-      messagesList.push (<Message key = {message.id} message={message} user={users[message.userId]} isMyMessage={isMyMessage}/>);
+  scrollTolastMessage=()=>{
+      var objDiv = document.getElementsByClassName('panel-body');
+      document.getElementsByClassName('panel-body')[0].scrollTop = document.getElementsByClassName('panel-body')[0].scrollHeight;
     }
-    return (
-      <div className="panel-body">
-        <ul className="chat">
-          {messagesList}
-        </ul>
-      </div>
-    );
+
+
+    render() {
+      if( !this.state.users || !this.state.messages )
+      {
+        return <p>MessagesList Loading....</p>
+      }
+      else {
+        if(Object.keys(this.state.messages).length==0)
+        {
+           return <p>Нет ни одного сообщения. Напишите первым !</p>
+        }
+        //console.log(this.props.data);
+        let currentUserId=this.state.currentUserId;
+        let users=this.state.users;
+        let messages=this.state.messages;
+        let messagesListView= new Array;
+
+        for (let prop in messages)
+        {
+          let message=messages[prop];
+
+          let isMyMessage=false;
+          if(message.userId==currentUserId){
+            isMyMessage=true;
+          }
+          messagesListView.push (<Message key = {message.id} message={message} user={users[message.userId]} isMyMessage={isMyMessage}/>);
+        }
+
+        return (
+          <div>
+            <div className="panel-body">
+              <ul className="chat">
+                {messagesListView}
+              </ul>
+            </div>
+
+          </div>
+
+        );
+      }
   }
 }
 
