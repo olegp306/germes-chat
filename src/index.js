1461c7540a87e7197f8eb6601950cad691dec349
olegp306 <olegp306@gmail.com>
2017-12-27 16:49:50 +0600

    Add signalr-no-jquery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { hubConnection } from 'signalr-no-jquery';
 
 import App from './App';
 
@@ -13,21 +14,41 @@ console.log("Запуск GermesChat index.js");
 //Иначе компонент попытаеться разу где-то отрисоваться
 
 
-// 2375300449000 Веребейчик Вадим Алексеевич
-//	2767852694000 ПСН API
-// 39098772000  Быконя
+// 2375300449000 Веребейчик Вадим Алексеевич chatId 2768027587000
+//	2767852694000 ПСН API chatId 2768027587000
+// 39098772000  Быконя chatId 2768027587000
+// 2690192400000 абармова chatId  2768089107000
+// 2767267939000 Медведева
 
-// export const  chatparams={
-//   'chatType':'',
-//   'chatId':'2768027587000',
-//   'userId':'2375300449000'
-// };
+export const  chatparams={
+  'chatType':'',
+  'chatId':'2768089346000',
+  'userId':'2767267939000'
+};
+
 //
-//
-// ReactDOM.render(
-//   React.createElement(App, {chatparams:chatparams}, null),
-//   document.getElementById('germes-chat')
-// );
+const pushServiceUrl="http://localhost:89/";
+const connection = hubConnection(pushServiceUrl);
+const hubProxy = connection.createHubProxy('RequestsHub');
+connection.logging = true;
+
+hubProxy.on('sendNewRequests', function(requests) {
+    console.log('sendNewRequests invoked', requests);
+});
+
+
+connection.start()
+.done(function(){ console.log('Now connected, connection ID=' + connection.id); })
+.fail(function(){ console.log('Could not connect'); });
+
+
+window.hubProxy=hubProxy;
+
+
+ReactDOM.render(
+  React.createElement(App, {chatparams:chatparams}, null),
+  document.getElementById('germes-chat')
+);
 
 // ReactDOM.render(<App chatparams={chatparams:chatparams}/>, document.getElementById('germes-chat'));
 
